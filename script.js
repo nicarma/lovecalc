b5c90048fc79b974509d46a995d06273beff4f36
nicarma <nicole_traut@gmx.de>
2022-03-10 20:13:29 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+prompt ("What's your name?");
+prompt ("What's their name?");
+
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1;
+
+if (loveScore > 70) {
+  alert("Your love score is " + loveScore + "%. You were made for each other <3");
+}
+else {
+ alert ("Your lovescore is: " + lovescore + "%");
+}
